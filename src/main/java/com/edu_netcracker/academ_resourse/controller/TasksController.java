b7086fad29b508fb6c288cfd9404942410c08928
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-11-23 00:44:03 +0300

    added ability to add tasks only for administrator

--- src/main/java/com/edu_netcracker/academ_resourse/controller/TasksController.java
+++ src/main/java/com/edu_netcracker/academ_resourse/controller/TasksController.java
@@ -1,8 +1,11 @@
 package com.edu_netcracker.academ_resourse.controller;
 
+import com.edu_netcracker.academ_resourse.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -10,7 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TasksController {
     private final static Logger logger = LoggerFactory.getLogger(TasksController.class);
     @GetMapping("/tasks")
-    public String getSchedule() {
+    public String getSchedule(@AuthenticationPrincipal User user, Model model) {
+        String isAdmin = new String();
+        if(user.getRole().equals("Admin"))
+            isAdmin = "<template v-if=\"true\">";
+        else if(user.getRole().equals("User"))
+            isAdmin = "<template v-if=\"false\">";
+        model.addAttribute("admin", isAdmin);
         return "tasks";
     }
 
