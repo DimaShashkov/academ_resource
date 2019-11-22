1fd5c4b630e365fff3f3c1dd123eca029ac64635
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-11-22 11:53:54 +0300

    added support for loading the schedule page in real time

--- src/main/java/com/edu_netcracker/academ_resourse/controller/UserController.java
+++ src/main/java/com/edu_netcracker/academ_resourse/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.edu_netcracker.academ_resourse.controller;
 
-import com.edu_netcracker.academ_resourse.domain.Group;
 import com.edu_netcracker.academ_resourse.domain.User;
-import com.edu_netcracker.academ_resourse.repos.IGroupRepo;
-import com.edu_netcracker.academ_resourse.repos.IUserRepo;
 import com.edu_netcracker.academ_resourse.schedule.factory.MongoGroupFactory;
 import com.edu_netcracker.academ_resourse.schedule.logic.JsoupPars;
 import com.edu_netcracker.academ_resourse.schedule.model.MongoGroup;
 import com.edu_netcracker.academ_resourse.services.GroupService;
-import com.edu_netcracker.academ_resourse.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
@@ -28,13 +23,6 @@ public class UserController {
     @Autowired
 	JsoupPars jsoupPars;
 
-    @Autowired
-	private IUserRepo userRepo;
-	@Autowired
-	private IGroupRepo groupRepo;
-
-	@Autowired
-	private UserService userService;
 	@Autowired
 	private GroupService groupService;
 
@@ -56,7 +44,6 @@ public class UserController {
 			@RequestParam String password,
 			@RequestParam String group,
 			@RequestParam String university,
-			HttpServletResponse response,
 			Model model) {
 
 		user.setEmail(email);
@@ -69,21 +56,14 @@ public class UserController {
 
 		MongoGroup mongoGroup = MongoGroupFactory.getGroup(university, group);
 		addSchedule(mongoGroup);
-		StringBuilder sb = new StringBuilder();
-		sb.append("http://localhost:8080/schedule?univ=").append(university).append("&group=").append(group.toUpperCase());
-		try {
-			response.sendRedirect(sb.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "profile";
+
+		return "redirect:/schedule";
 	}
 
 	private void addSchedule(MongoGroup mongoGroup) {
 		try {
 			jsoupPars.addSchedule(mongoGroup);
 		} catch (IOException e) {
-			e.printStackTrace();
 			logger.error("schedule wasn't added into Mongo: ", e);
 		}
 		logger.info("the data has been added into model");
