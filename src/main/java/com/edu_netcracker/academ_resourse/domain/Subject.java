44676feaf487908806e2b3e2bfc1eef9a7ece8a0
shdma <shashkovdima981@gmail.com>
2019-11-25 11:10:33 +0400

    add Entity subject

---
+++ src/main/java/com/edu_netcracker/academ_resourse/domain/Subject.java
@@ -0,0 +1,36 @@
+package com.edu_netcracker.academ_resourse.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subject")
+public class Subject {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String name;
+
+	public Subject() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
