223553ed51a4085214879b855326008f52c66f1b
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-12-12 00:47:40 +0300

    tasks on the calendar page are now displayed from vue js

--- src/main/java/com/edu_netcracker/academ_resourse/controller/CalendarController.java
+++ src/main/java/com/edu_netcracker/academ_resourse/controller/CalendarController.java
@@ -3,20 +3,23 @@ package com.edu_netcracker.academ_resourse.controller;
 import com.edu_netcracker.academ_resourse.domain.Task;
 import com.edu_netcracker.academ_resourse.domain.User;
 import com.edu_netcracker.academ_resourse.services.TasksService;
+import com.google.gson.Gson;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import java.util.Set;
 
 @Controller
 public class CalendarController {
 
-    final
-    TasksService tasksService;
+    final TasksService tasksService;
 
-    public CalendarController(TasksService tasksService) {
+    public CalendarController(TasksService tasksService, Gson gson) {
         this.tasksService = tasksService;
     }
 
@@ -49,5 +52,11 @@ public class CalendarController {
         model.addAttribute("novemberTask", novemberTask);
         model.addAttribute("decemberTask", decemberTask);
         return "calendar";
+    }
+
+    @GetMapping("/calendar/tasks")
+    @ResponseBody
+    public Set<Task> getTasksJson(@AuthenticationPrincipal User user) {
+        return user.getGroup().getTasks();
     }
 }
--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,6 +1,10 @@
+var taskApi = Vue.resource("/calendar/tasks");
+
+
 var calendar = new Vue({
     el: '#calendar',
     data: {
+        tasks: [],
         title: 'Calendar',
         months: moment().months(),
         today:moment().format("D"),
@@ -14,50 +18,48 @@ var calendar = new Vue({
         thursday:'Thursday',
         counter: 0,
     },
+    created: function() {
+      taskApi.get().then(result =>
+      result.json().then(data => data.forEach( task => this.tasks.push(task)))
+      );
+        taskApi.get().then(result =>
+            result.json().then(data => data.forEach( task => console.log(task)))
+        );
+    },
     methods: {
         previousMonth: function() {
             this.counter--
             this.thisMonth = moment().add(this.counter, 'months').format('MMMM');
-            taskList.thisMonth = this.thisMonth;
             this.lastDay =  parseInt(moment().add(this.counter, 'months').endOf('month').format('D'));
             this.thisYear = moment().add(this.counter, 'months').format("YYYY");
-            taskList.thisYear = this.thisYear;
-            taskList.year();
             this.firstWeekDay= moment().add(this.counter, 'months').startOf('month').format("dddd");
             console.log(this.thisMonth)
         },
         nextMonth: function() {
             this.counter++
             this.thisMonth = moment().add(this.counter, 'months').format('MMMM');
-            taskList.thisMonth = this.thisMonth;
             this.lastDay =  parseInt(moment().add(this.counter, 'months').endOf('month').format('D'));
             this.thisYear = moment().add(this.counter, 'months').format("YYYY");
-            taskList.thisYear = this.thisYear;
-            taskList.year();
             this.firstWeekDay= moment().add(this.counter, 'months').startOf('month').format("dddd");
             console.log(this.thisMonth)
-        }
-    },
-});
+        },
+        currentDate: function(n, date) {
+            this.monthForTasks = moment().add(this.counter, 'months').format('MM');
+            // console.log(this.monthForTasks);
+            // console.log(this.thisYear);
+            // console.log(n);
+            // console.log(date);
 
-var taskList = new Vue({
-    el: '#taskList',
-    data: {
-        thisMonth: calendar.thisMonth,
-        thisYear: calendar.thisYear,
-        firstSemesterYear: calendar.thisYear,
-        secondSemesterYear: calendar.thisYear
-    },
-    methods: {
-        year: function () {
-            if (moment().add(0, 'months').format('MM') > 9) {
-                this.firstSemesterYear = moment().add(0, 'months').format("YYYY");
-                this.secondSemesterYear = moment().add(1, 'months').format("YYYY");
+            dateA = moment(this.monthForTasks + '-' + n + '-' + this.thisYear, "MM-DD-YYYY");
+            dateB = moment(date, "MM-DD-YYYY");
+            console.log(dateA);
+            console.log(dateB);
+            if (moment(this.thisYear + '-' + this.monthForTasks + '-' + n).isSame(date)) {
+                return true;
             } else {
-                this.firstSemesterYear = moment().add(-1, 'months').format("YYYY");
-                this.secondSemesterYear = moment().add(0, 'months').format("YYYY");
+                return false;
             }
         }
-    }
-
+    },
 });
+
