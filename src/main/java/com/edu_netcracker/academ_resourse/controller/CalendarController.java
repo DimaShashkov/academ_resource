2bc3c886350df988faf318667e559cc9ae9623fa
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-12-13 06:19:23 +0300

    some changes into calendar page

--- src/main/java/com/edu_netcracker/academ_resourse/controller/CalendarController.java
+++ src/main/java/com/edu_netcracker/academ_resourse/controller/CalendarController.java
@@ -24,33 +24,7 @@ public class CalendarController {
     }
 
     @GetMapping(value = "/calendar")
-    public String getController(@AuthenticationPrincipal User user, Model model) {
-        Set<Task> tasks = user.getGroup().getTasks();
-        Set<Task> januaryTask = tasksService.getJanuaryTasks(tasks);
-        Set<Task> februaryTask = tasksService.getFebruaryTasks(tasks);
-        Set<Task> marchTask = tasksService.getMarchTasks(tasks);
-        Set<Task> aprilTask = tasksService.getAprilTasks(tasks);
-        Set<Task> mayTask = tasksService.getMayTasks(tasks);
-        Set<Task> juneTask = tasksService.getJuneTasks(tasks);
-        Set<Task> julyTask = tasksService.getJulyTasks(tasks);
-        Set<Task> augustTask = tasksService.getAugustTasks(tasks);
-        Set<Task> septemberTask = tasksService.getSeptemberTasks(tasks);
-        Set<Task> octoberTask = tasksService.getOctoberTasks(tasks);
-        Set<Task> novemberTask = tasksService.getNovemberTasks(tasks);
-        Set<Task> decemberTask = tasksService.getDecemberTasks(tasks);
-
-        model.addAttribute("januaryTask", januaryTask);
-        model.addAttribute("februaryTask", februaryTask);
-        model.addAttribute("marchTask", marchTask);
-        model.addAttribute("aprilTask", aprilTask);
-        model.addAttribute("mayTask", mayTask);
-        model.addAttribute("juneTask", juneTask);
-        model.addAttribute("julyTask", julyTask);
-        model.addAttribute("augustTask", augustTask);
-        model.addAttribute("septemberTask", septemberTask);
-        model.addAttribute("octoberTask", octoberTask);
-        model.addAttribute("novemberTask", novemberTask);
-        model.addAttribute("decemberTask", decemberTask);
+    public String getController() {
         return "calendar";
     }
 
--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,13 +1,27 @@
 var taskApi = Vue.resource("/calendar/tasks");
 
 
+Vue.component('modal', {
+    template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box"><slot></slot></div>
+      </div>
+      <button class="modal-close" @click="$emit('close')"></button>
+    </div>
+  `
+});
+
 var calendar = new Vue({
     el: '#calendar',
     data: {
         tasks: [],
+        monthForTasks : moment().format("MM"),
         title: 'Calendar',
         months: moment().months(),
         today:moment().format("D"),
+        realMonth:moment().format("MMMM"),
         thisMonth:moment().format("MMMM"),
         thisYear:moment().format("YYYY"),
         weekDays: moment.weekdays(),
@@ -17,11 +31,42 @@ var calendar = new Vue({
         friday:'Friday',
         thursday:'Thursday',
         counter: 0,
+        showModal1: false,
+        showModal: false,
+        showModal3: false,
+        showModal4: false,
+        showModal5: false,
+        showModal6: false,
+        showModal7: false,
+        showModal8: false,
+        showModal9: false,
+        showModal10: false,
+        showModal11: false,
+        showModal12: false,
+        showModal13: false,
+        showModal14: false,
+        showModal15: false,
+        showModal16: false,
+        showModal17: false,
+        showModal18: false,
+        showModal19: false,
+        showModal20: false,
+        showModal21: false,
+        showModal22: false,
+        showModal23: false,
+        showModal24: false,
+        showModal25: false,
+        showModal26: false,
+        showModal27: false,
+        showModal28: false,
+        showModal29: false,
+        showModal30: false,
+        showModal31: false,
     },
     created: function() {
-      taskApi.get().then(result =>
-      result.json().then(data => data.forEach( task => this.tasks.push(task)))
-      );
+        taskApi.get().then(result =>
+            result.json().then(data => data.forEach( task => this.tasks.push(task)))
+        );
         taskApi.get().then(result =>
             result.json().then(data => data.forEach( task => console.log(task)))
         );
@@ -52,13 +97,21 @@ var calendar = new Vue({
 
             dateA = moment(this.monthForTasks + '-' + n + '-' + this.thisYear, "MM-DD-YYYY");
             dateB = moment(date, "MM-DD-YYYY");
-            console.log(dateA);
-            console.log(dateB);
+            // console.log(dateA);
+            // console.log(dateB);
             if (moment(this.thisYear + '-' + this.monthForTasks + '-' + n).isSame(date)) {
                 return true;
             } else {
                 return false;
             }
+        },
+        buttonForTasks: function(n) {
+            return '<button class="button is-success" @click="showModal' + n +  '= true">' +
+                'show tasks' +
+                '</button>'
+        },
+        modalForTasks: function(n) {
+            return '<modal title="Title of the modal" v-show="showModal' + n + '" @close="showModal' + n + ' = false">'
         }
     },
 });
