2ff6191188e53d8dac62cc33f540809a99531389
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-12-14 21:24:54 +0300

    new style into tasks page

--- src/main/resources/static/js/tasks.js
+++ src/main/resources/static/js/tasks.js
@@ -1,6 +1,27 @@
 var app2 = new Vue({
+    el: '#addTaskButton',
+    data: {
+        isAddTask: false,
+    },
+    methods: {
+        setIsAddTask: function () {
+            this.isAddTask = true;
+        }
+    },
+});
+var app1 = new Vue({
     el: '#newJob',
     data: {
-        access: true
-    }
+        access: true,
+    },
+    methods: {
+        showTask: function () {
+        if(app2.isAddTask === true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+    },
 });
