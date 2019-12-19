5852353e1c7852f8cd75ba294829828ee9773a03
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2019-12-19 03:32:32 +0300

    full Russian localization on the profile page

--- src/main/java/com/edu_netcracker/academ_resourse/schedule/factory/MongoGroupFactory.java
+++ src/main/java/com/edu_netcracker/academ_resourse/schedule/factory/MongoGroupFactory.java
@@ -8,19 +8,19 @@ import com.edu_netcracker.academ_resourse.schedule.universities.Smtu;
 public class MongoGroupFactory {
 
     public static MongoGroup getGroup(final String university, final String group) {
-        if(university.equals("ITMO")) {
+        if(university.equalsIgnoreCase("ITMO")) {
             MongoGroup<Itmo> mongoGroup = new MongoGroup();
             mongoGroup.setUniversity(new Itmo(group));
             mongoGroup.setGroup(group);
             return mongoGroup;
         }
-        else if(university.equals("SMTU")){
+        else if(university.equalsIgnoreCase("SMTU")){
             MongoGroup<Smtu> mongoGroup = new MongoGroup();
             mongoGroup.setUniversity(new Smtu(group));
             mongoGroup.setGroup(group);
             return mongoGroup;
         }
-        else if(university.equals("NSU")){
+        else if(university.equalsIgnoreCase("NSU")){
             MongoGroup<Nsu> mongoGroup = new MongoGroup();
             mongoGroup.setUniversity(new Nsu(group));
             mongoGroup.setGroup(group);
